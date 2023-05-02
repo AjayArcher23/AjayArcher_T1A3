748b73b524a7b92c19a91cf7202a24fe155ca197
AjayArcher23 <ajayarcherr@gmail.com>
2023-05-02 20:45:50 +1000

    function created which finds a random position in the puzzle, checks if words will fit and places them in a random direction

--- Terminal_App_Code/main.py
+++ Terminal_App_Code/main.py
@@ -1,4 +1,14 @@
 import random 
+rowl = 10 
+coll = 10
+
+
+game_finished = False 
+level1_finished = False 
+level2_finished = False 
+level3_finished = False 
+level4_finished = False 
+level5_finished = False 
 
 # Step 1: Themes and words
 
@@ -6,31 +16,54 @@ class Wordsearch:
     def __init__(self, theme, words): 
         self.theme = theme
         self.words = words 
-    def __str__(self): 
+    def theme_word_display(self): 
         return f"""{self.theme} 
 {self.words}"""
 
 # Level 1
-wordsearch_easiest = Wordsearch( "Animals: ", ["cat", "dog", "frog", "tiger", "lizard", "penguin", "elephant"])  
+wordsearch_easiest = Wordsearch( "Animals: ", ["CAT", "FROG", "TIGER", "LIZARD", "PENGUIN", "ELEPHANT"])  
 print (wordsearch_easiest) 
 # Level 2
-wordsearch_easy = Wordsearch( "Sports: ", ["basketball", "football", "tennis", "baseball", "hockey", "golf", "boxing"])  
+wordsearch_easy = Wordsearch( "Sports: ", ["BASKETBALL", "FOOTBALL", "TENNIS", "BASEBALL", "HOCKEY", "GOLF", "BOXING"])  
 print (wordsearch_easy) 
 # Level 3
-wordsearch_medium = Wordsearch( "Capital Cities: ", ["london", "paris", "tokyo", "washington", "Amsterdam", "canberra", "rome"])  
+wordsearch_medium = Wordsearch( "Capital Cities: ", ["LONDON", "PARIS", "TOKYO", "WASHINGTON", "AMSTERDAM", "CANBERRA", "ROME"])  
 print (wordsearch_medium) 
 # Level 4
-wordsearch_hard = Wordsearch( "Music Genres: ", ["rock", "rap", "jazz", "pop", "classical", "country", "reggae"])  
+wordsearch_hard = Wordsearch( "Music Genres: ", ["ROCK", "RAP", "JAZZ", "POP", "CLASSICAL", "COUNTRY", "REGGAE"])  
 print (wordsearch_hard) 
 # Level 5
-wordsearch_hardest = Wordsearch( "Python: ", ["loop", "conditional", "function", "class", "object", "input"])  
+wordsearch_hardest = Wordsearch( "Python: ", ["LOOP", "CONDITIONAL", "FUNCTION", "CLASS", "OBJECT", "INPUT"])  
 print (wordsearch_hardest)
         
-# List for letters 
+# 2D List of letters  
+puzzle = [["" for _ in range(10)] for _ in range(10)]   
 
-# Place chosen words in puzzle 
+# Place chosen words in puzzle   
+def word_placer(words):
+    for word in words: 
+        # Selects random position in 2d list
+        row, col = random.randint(0, 9), random.randint(0, 9)  
+        # Selects the direction the word will go in
+        direction = random.choice("hor", "ver") 
+        if direction == "hor":  
+            # Checks word will fit 
+            if col + len(word) > 10: 
+                col = 10 - len(word)  
+                # Places word in random position (going horizontal)
+                for i in range(len(word)):
+                    puzzle[row][col+i] = word[i]  
+        else: 
+            if row + len(word) > 10: 
+                row = 10 - len(word) 
+                # Places word in random position (going vertical)
+                for i in range(len(word)): 
+                    puzzle[row+i][col] = word[i]
+
 
 # Fill empty spaces with random letters 
+# def fill_grid(puzzle):  
+
 
 # Display the puzzle 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-# AjayArcher_T1A3
+# Word Search Terminal Application 
+
+## Features 
+. Word Search Puzzle
+. Skip Level Feature 
+. List of unfound words "......"  
+. List of found words   "Search"  
+. Visually pleasing ui 
+
+## List of tasks 
+. Find/Create list of letters and words for word searches. 
+. Research the best way to print list to terminal. 
+. create list of user requirements at each stage of application
+. 
