adf8985d48273c9a4fecc0f27973b2fcbf977210
AjayArcher23 <ajayarcherr@gmail.com>
2023-05-04 16:04:43 +1000

    Words can now be placed diagonally in the puzzle and won't overlap

--- Terminal_App_Code/main.py
+++ Terminal_App_Code/main.py
@@ -1,7 +1,8 @@
 import random 
-lvlrow = 10 
-lvlcol = 10
+lvlrowcol = 10 
 
+# Creates 2D list
+puzzle = [['' for _ in range(lvlrowcol)] for _ in range(lvlrowcol)]
 
 game_finished = False 
 
@@ -12,8 +13,68 @@ class Levels:
         self.theme = theme 
         self. words = words 
 
-    def run(self): 
-        print(f"The theme {self.theme} contains the words {self.words}")
+    def run(self, words): 
+        print(f"The theme {self.theme} contains the words {self.words}") 
+        # Places words in the puzzle
+        for word in words:
+            while True:
+                x, y = random.randint(0, lvlrowcol), random.randint(0, lvlrowcol)
+                direction = random.choice(['horizontal', 'vertical', 'diagonal_down', 'diagonal_up'])
+                if direction == 'horizontal':
+                    if y + len(word) > lvlrowcol:
+                        continue
+                    overlap = False
+                    for i in range(len(word)):
+                        if puzzle[x][y+i] != '':
+                            overlap = True
+                            break
+                    if overlap:
+                        continue
+                    for i in range(len(word)):
+                        puzzle[x][y+i] = word[i]
+                    break
+                elif direction == 'vertical':
+                    if x + len(word) > lvlrowcol:
+                        continue
+                    overlap = False
+                    for i in range(len(word)):
+                        if puzzle[x+i][y] != '':
+                            overlap = True
+                            break
+                    if overlap:
+                        continue
+                    for i in range(len(word)):
+                        puzzle[x+i][y] = word[i]
+                    break
+                elif direction == 'diagonal_down':
+                    if x + len(word) > lvlrowcol or y + len(word) > lvlrowcol:
+                        continue
+                    overlap = False
+                    for i in range(len(word)):
+                        if puzzle[x+i][y+i] != '':
+                            overlap = True
+                            break
+                    if overlap:
+                        continue
+                    for i in range(len(word)):
+                        puzzle[x+i][y+i] = word[i]
+                    break
+                elif direction == 'diagonal_up':
+                    if x - len(word) < 0 or y + len(word) > lvlrowcol:
+                        continue
+                    overlap = False
+                    for i in range(len(word)):
+                        if puzzle[x-i][y+i] != '':
+                            overlap = True
+                            break
+                    if overlap:
+                        continue
+                    for i in range(len(word)):
+                        puzzle[x-i][y+i] = word[i]
+                    break  
+
+       
+            
 
 
 # Level 1
