ccaebe7953cd4b9bc67699817e7b04db16c41799
AjayArcher23 <ajayarcherr@gmail.com>
2023-05-07 12:56:58 +1000

    put the lists of words into one list called 'levels', created the main game loops which runs all functions/handles output, currently the game will run through all levels with some bugs

--- Terminal_App_Code/main.py
+++ Terminal_App_Code/main.py
@@ -1,148 +1,148 @@
-import random 
-lvlrowcol = 10 
-game_finished = False
+import random
 
+lvlrowcol = 10
 
-# Level 1
-wordsearch_easiest = ["CAT","TIGER","LIZARD","PENGUIN","ELEPHANT"]  
-# Level 2
-wordsearch_easy = ["BASKETBALL","FOOTBALL","TENNIS","BASEBALL","HOCKEY","GOLF","BOXING"]
-# Level 3
-wordsearch_medium = ["LONDON","PARIS","TOKYO","WASHINGTON","AMSTERDAM","CANBERRA","ROME"]  
-# Level 4
-wordsearch_hard = ["ROCK","RAP","JAZZ","POP","CLASSICAL","COUNTRY","REGGAE"]   
-# Level 5
-wordsearch_hardest = ["LOOP","CONDITIONAL","FUNCTION","CLASS","OBJECT","INPUT"]
-
-words = wordsearch_easiest
+levels = [
+    ["DOG", "TIGER", "LIZARD", "PENGUIN", "ELEPHANT"],
+    ["BASKETBALL", "FOOTBALL", "TENNIS", "BASEBALL", "HOCKEY", "GOLF", "BOXING"],
+    ["LONDON", "PARIS", "TOKYO", "WASHINGTON", "AMSTERDAM", "CANBERRA", "ROME"],
+    ["ROCK", "RAP", "JAZZ", "POP", "CLASSICAL", "COUNTRY", "REGGAE"],
+    ["LOOP", "CONDITIONAL", "FUNCTION", "CLASS", "OBJECT", "INPUT"]
+]
 
 # 2D list for letters
 puzzle = [['' for _ in range(lvlrowcol)] for _ in range(lvlrowcol)]
 
- # Places words in the puzzle
-def run():  
-        for word in words:
-            while True:
-                x, y = random.randint(0, lvlrowcol-1), random.randint(0, lvlrowcol-1)
-                direction = random.choice(['horizontal', 'vertical', 'diagonal_down', 'diagonal_up'])
-                if direction == 'horizontal':
-                    if y + len(word) > lvlrowcol:
-                        continue
-                    overlap = False
-                    for i in range(len(word)):
-                        if puzzle[x][y+i] != '':
-                            overlap = True
-                            break
-                    if overlap:
-                        continue
-                    for i in range(len(word)):
-                        puzzle[x][y+i] = word[i]
-                    break
-                elif direction == 'vertical':
-                    if x + len(word) > lvlrowcol:
-                        continue
-                    overlap = False
-                    for i in range(len(word)):
-                        if puzzle[x+i][y] != '':
-                            overlap = True
-                            break
-                    if overlap:
-                        continue
-                    for i in range(len(word)):
-                        puzzle[x+i][y] = word[i]
-                    break
-                elif direction == 'diagonal_down':
-                    if x + len(word) > lvlrowcol or y + len(word) > lvlrowcol:
-                        continue
-                    overlap = False
-                    for i in range(len(word)):
-                        if puzzle[x+i][y+i] != '':
-                            overlap = True
-                            break
-                    if overlap:
-                        continue
-                    for i in range(len(word)):
-                        puzzle[x+i][y+i] = word[i]
-                    break
-                elif direction == 'diagonal_up':
-                    if x - len(word) < 0 or y + len(word) > lvlrowcol:
-                        continue
-                    overlap = False
-                    for i in range(len(word)):
-                        if puzzle[x-i][y+i] != '':
-                            overlap = True
-                            break
-                    if overlap:
-                        continue
-                    for i in range(len(word)):
-                        puzzle[x-i][y+i] = word[i]
-                    break
+#Places words in the puzzle
+def place_words(words): 
+    for word in words:
+        while True:
+            x, y = random.randint(0, lvlrowcol - 1), random.randint(0, lvlrowcol - 1)
+            direction = random.choice(['horizontal', 'vertical', 'diagonal_down', 'diagonal_up'])
+            if direction == 'horizontal':
+                if y + len(word) > lvlrowcol:
+                    continue
+                overlap = False
+                for i in range(len(word)):
+                    if puzzle[x][y + i] != '':
+                        overlap = True
+                        break
+                if overlap:
+                    continue
+                for i in range(len(word)):
+                    puzzle[x][y + i] = word[i]
+                break
+            elif direction == 'vertical':
+                if x + len(word) > lvlrowcol:
+                    continue
+                overlap = False
+                for i in range(len(word)):
+                    if puzzle[x + i][y] != '':
+                        overlap = True
+                        break
+                if overlap:
+                    continue
+                for i in range(len(word)):
+                    puzzle[x + i][y] = word[i]
+                break
+            elif direction == 'diagonal_down':
+                if x + len(word) > lvlrowcol or y + len(word) > lvlrowcol:
+                    continue
+                overlap = False
+                for i in range(len(word)):
+                    if puzzle[x + i][y + i] != '':
+                        overlap = True
+                        break
+                if overlap:
+                    continue
+                for i in range(len(word)):
+                    puzzle[x + i][y + i] = word[i]
+                break
+            elif direction == 'diagonal_up':
+                if x - len(word) < 0 or y + len(word) > lvlrowcol:
+                    continue
+                overlap = False
+                for i in range(len(word)):
+                    if puzzle[x - i][y + i] != '':
+                        overlap = True
+                        break
+                if overlap:
+                    continue
+                for i in range(len(word)):
+                    puzzle[x - i][y + i] = word[i]
+                break
 
-        #Fills empty spaces with random letters
-        for i in range(lvlrowcol):
-            for j in range(lvlrowcol):
-                if puzzle[i][j] == '':
-                    puzzle[i][j] = chr(random.randint(65, 90))
+# Fills empty spaces with random letters
+def fill_puzzle():
+    for i in range(lvlrowcol):
+        for j in range(lvlrowcol):
+            if puzzle[i][j] == '':
+                puzzle[i][j] = chr(random.randint(65, 90))
 
-        # Displays the puzzle
-        def display_puzzle():
-            for row in puzzle:
-                print(' '.join(row))
 
+# Displays the puzzle
+def display_puzzle():
+    for row in puzzle:
+        print(' '.join(row))
+
+# Searches puzzle for the word
+def search_word(word):
+    # Check horizontal lines
+    for row in puzzle:
+        if word in ''.join(row):
+            return True
+    # Check vertical lines
+    for i in range(lvlrowcol):
+        if word in ''.join([row[i] for row in puzzle]):
+            return True 
+    # Check diagonal lines
+    for i in range(lvlrowcol - len(word) + 1):
+        for j in range(lvlrowcol - len(word) + 1):
+            if word == ''.join([puzzle[i+k][j+k] for k in range(len(word))] ):
+                return True
+            elif word == ''.join([puzzle[i+k][j+len(word)-1-k] for k in range(len(word)-1, -1, -1)]):
+                return True
+
+    return False
+
+# Prompts the user for a word
+def get_user_input():
+    return input("Enter a word to search for (or type 'QUIT' to exit): ").upper()
+
+# Main Game Loop
+def play_game():
+    global puzzle
+    print("Welcome to the word search game!")
+    quit_game = False
+    for level in range(1, 6):
+        print(f"Level {level}")
+        puzzle = [['' for _ in range(lvlrowcol)] for _ in range(lvlrowcol)]
+        words = levels[level - 1]
+        place_words(words)
+        fill_puzzle()
         display_puzzle()
-
-        # Prompts the user for a word
-        def get_user_input():
-            return input("Enter a word to search for: ").upper()
-
-        # Searches puzzle for the word
-        def search_word(word):
-            found = False
-            # Horizontal Search
-            for i in range(lvlrowcol):
-                row = ''.join(puzzle[i])
-                if word in row:
-                    found = True
-                    break
-            # Vertical Search
-            if not found:
-                for j in range(lvlrowcol):
-                    col = ''.join([puzzle[i][j] for i in range(10)])
-                    if word in col:
-                        found = True
-                        break
-            # Diagonal search from top-left to bottom-right
-            if not found:
-                for i in range(lvlrowcol):
-                    for j in range(lvlrowcol):
-                        if puzzle[i][j] == word[0]:
-                            for k in range(len(word)):
-                                if i+k < lvlrowcol and j+k < lvlrowcol and puzzle[i+k][j+k] == word[k]:
-                                    found = True
-                                else:
-                                    found = False
-                                    break
-                            if found:
-                                break
-                    if found:
-                        break
-            return found
-
-        # Displays the result of search
         found_words = []
-        while len(found_words) < len(words):
-            word_to_find = get_user_input()
-            if word_to_find.upper() in words and word_to_find.upper() not in found_words:
-                if search_word(word_to_find.upper()):
-                    print("Word found!")
-                    found_words.append(word_to_find.upper())
-            elif word_to_find.upper() in found_words:
-                print("You've already found that word.") 
-            else:
-                print("Word not found, try again.")
-                
-            if len(found_words) == len(words):
-                print("Congratulations! You found all the words.") 
-run()
-
-
+        for word in words:
+            found = False
+            while not found:
+                user_input = get_user_input() 
+                print(user_input)
+                if user_input.startswith("LEVEL ") and user_input[6:] == str(level + 1):
+                    break
+                elif user_input == "QUIT":
+                    quit_game = True
+                    break
+                elif search_word(user_input) and user_input == word and user_input not in found_words:
+                    print("Congratulations! You found a word!")
+                    found = True 
+                    found_words.append(word)
+                elif search_word(user_input):
+                    print("You've already found that word! Try again.")
+                else:
+                    print("Sorry, that word is not in the puzzle. Try again.")
+        if quit_game:
+            break
+    print("Thanks for playing!") 
+    
+if __name__ == '__main__':
+    play_game()
